07ee71367f7bf20095067892c9c3f58b5bf307de
Zeenobit <zeenobit.exe@gmail.com>
2024-07-26 13:08:07 -0400

    Move `Memory` into memory.rs

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ use std::{
 
 use bevy_app::{App, Plugin};
 use bevy_ecs::{prelude::*, query::QueryData};
-use bevy_reflect::{FromReflect, GetTypeRegistration, Reflect, TypePath};
+use bevy_reflect::{FromReflect, GetTypeRegistration, TypePath};
 use bevy_utils::tracing::warn;
 use moonshine_util::future::Future;
 
@@ -427,59 +427,10 @@ impl<B: Behavior> BorrowMut<B> for BehaviorMutItem<'_, B> {
     }
 }
 
-/// A [`Component`] which stores a stack of paused [`Behavior`] states to be resumed later.
-#[derive(Component, Clone, Reflect)]
-#[reflect(Component)]
-pub struct Memory<B: Behavior>(Vec<B>);
-
-impl<B: Behavior> Memory<B> {
-    /// Returns the number of paused [`Behavior`] states in the stack.
-    pub fn len(&self) -> usize {
-        self.0.len()
-    }
-
-    /// Returns `true` if the stack is empty.
-    pub fn is_empty(&self) -> bool {
-        self.0.is_empty()
-    }
-
-    /// Returns an iterator over the paused [`Behavior`] states in the stack.
-    ///
-    /// The iterator starts from the most recently paused state (previous).
-    pub fn iter(&self) -> impl Iterator<Item = &B> {
-        self.0.iter().rev()
-    }
-
-    /// Returns `true` if the stack contains the given [`Behavior`] state.
-    pub fn contains(&self, behavior: &B) -> bool
-    where
-        B: PartialEq,
-    {
-        self.0.contains(behavior)
-    }
-
-    /// Returns a reference to the previous [`Behavior`] state, if it exists.
-    pub fn previous(&self) -> Option<&B> {
-        self.0.last()
-    }
-
-    fn push(&mut self, behavior: B) {
-        self.0.push(behavior)
-    }
-
-    fn pop(&mut self) -> Option<B> {
-        self.0.pop()
-    }
-}
-
-impl<B: Behavior> Default for Memory<B> {
-    fn default() -> Self {
-        Self(Vec::new())
-    }
-}
-
 mod events;
+mod memory;
 mod transition;
 
 pub use events::*;
+pub use memory::*;
 pub use transition::*;
